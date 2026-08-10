3462ef17bdf0e319afb567ab1a76d1d1c4ae48bc
nguyentuanson27-netizen <nguyentuanson27@gmail.com>
2026-08-10 11:59:26 +0700

    fix: declare Node structuredClone for lint

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default [
         process: 'readonly',
         setImmediate: 'readonly',
         setTimeout: 'readonly',
+        structuredClone: 'readonly',
       },
     },
     rules: {
